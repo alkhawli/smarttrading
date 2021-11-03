561a6a1ac51838ddc69194c2bd950dc027f9cccb
tkw <toufik.alkhawli@umlaut.com>
2021-11-03 23:20:49 +0100

    new format

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -5,30 +5,36 @@ import re
 import string
 import contractions
 import pandas as pd
+
 nltk.download('vader_lexicon')
 
+
 def remove_linebreak(text):
-	linebreak_pattern = r'(\r\n)+|(\r)+|(\n)+|(\t)+'
-	without_linebreak_pattern = re.sub(pattern = linebreak_pattern, repl = " ", string = text)
-	return without_linebreak_pattern
+    linebreak_pattern = r'(\r\n)+|(\r)+|(\n)+|(\t)+'
+    without_linebreak_pattern = re.sub(pattern=linebreak_pattern, repl=" ", string=text)
+    return without_linebreak_pattern
+
 
 def remove_extra_spaces(text):
-	space_pattern = r"\s+"
-	text = re.sub(pattern = space_pattern, repl = " ", string = text)
-	space_pattern_left_right = r"(^\s|\s$)"
-	text = re.sub(pattern = space_pattern_left_right, repl = "", string = text)
-	return text
+    space_pattern = r"\s+"
+    text = re.sub(pattern=space_pattern, repl=" ", string=text)
+    space_pattern_left_right = r"(^\s|\s$)"
+    text = re.sub(pattern=space_pattern_left_right, repl="", string=text)
+    return text
+
 
 def lower_case_convertion(text):
-	lower_text = text.lower()
-	return lower_text
-    
+    lower_text = text.lower()
+    return lower_text
+
+
 def expand_contraction(text):
-	text = contractions.fix(text)
-	return text
+    text = contractions.fix(text)
+    return text
+
 
 def remove_emojis(text):
-	emoji_pattern = re.compile("["
+    emoji_pattern = re.compile("["
                                u"\U0001F600-\U0001F64F"  # emoticons
                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
@@ -48,74 +54,77 @@ def remove_emojis(text):
                                u"\ufe0f"  # dingbats
                                u"\u3030"
                                "]+", flags=re.UNICODE)
-                               
-	without_emoji = emoji_pattern.sub(r'',text)
-	return without_emoji
+
+    without_emoji = emoji_pattern.sub(r'', text)
+    return without_emoji
+
 
 def remove_punctuation(text):
-	return text.translate(str.maketrans("", "", string.punctuation))
+    return text.translate(str.maketrans("", "", string.punctuation))
+
 
 def text_preprocessing(
-	df,
-	column_name, 
-	remove_linebreak_pattern = True,
-	to_lower_case = False,
-	expand_contraction_pattern = True,
-	remove_emoji_pattern = True,
-	remove_punctuation_pattern = False
-	):
+        df,
+        column_name,
+        remove_linebreak_pattern=True,
+        to_lower_case=False,
+        expand_contraction_pattern=True,
+        remove_emoji_pattern=True,
+        remove_punctuation_pattern=False
+):
+    column_name_cleaned = f"{column_name}_cleaned".upper()
+    if remove_linebreak_pattern:
+        df[column_name_cleaned] = df[column_name].apply(remove_linebreak)
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
 
-	column_name_cleaned = f"{column_name}_cleaned".upper()
-	if remove_linebreak_pattern:
-		df[column_name_cleaned] = df[column_name].apply(remove_linebreak)
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
+    if to_lower_case:
+        df[column_name_cleaned] = df[column_name_cleaned].apply(lower_case_convertion)
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
 
-	if to_lower_case:
-		df[column_name_cleaned] = df[column_name_cleaned].apply(lower_case_convertion)
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
+    if expand_contraction_pattern:
+        df[column_name_cleaned] = df[column_name_cleaned].apply(expand_contraction)
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
 
-	if expand_contraction_pattern:
-		df[column_name_cleaned] = df[column_name_cleaned].apply(expand_contraction)
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
+    if remove_emoji_pattern:
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_emojis)
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
 
-	if remove_emoji_pattern:
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_emojis)
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
-
-	if remove_punctuation_pattern:
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_punctuation)
-		df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
-	return df
+    if remove_punctuation_pattern:
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_punctuation)
+        df[column_name_cleaned] = df[column_name_cleaned].apply(remove_extra_spaces)
+    return df
 
 
 with open("./output/top_tickers_v2.json", "r") as f:
-    x=json.load(f)
+    x = json.load(f)
 
 # This is only for the first array of comments
-comments=x['stock_list'][0].get("comments")
+comments = x['stock_list'][0].get("comments")
 df = pd.DataFrame(comments)
 
 df = text_preprocessing(
-    df = df, 
-    column_name = "body", 
-    remove_linebreak_pattern = True,
- 	to_lower_case = True,
- 	expand_contraction_pattern = True,
- 	remove_emoji_pattern = True,
- 	remove_punctuation_pattern = True
- 	)
+    df=df,
+    column_name="body",
+    remove_linebreak_pattern=True,
+    to_lower_case=True,
+    expand_contraction_pattern=True,
+    remove_emoji_pattern=True,
+    remove_punctuation_pattern=True
+)
 
-df['neg']=0
-df['neu']=0
-df['pos']=0
-df['compound']=0
+df['neg'] = 0
+df['neu'] = 0
+df['pos'] = 0
+df['compound'] = 0
 
 sid = SentimentIntensityAnalyzer()
+
 for index, row in df.iterrows():
     sentimentResults = sid.polarity_scores(row.BODY_CLEANED)
     print(sentimentResults)
-    df.neg.iloc[index]=sentimentResults['neg']
-    df.neu.iloc[index]=sentimentResults['neu']
-    df.pos.iloc[index]=sentimentResults['pos']
-    df.compound.iloc[index]=sentimentResults['compound']
+    df.neg.iloc[index] = sentimentResults['neg']
+    df.neu.iloc[index] = sentimentResults['neu']
+    df.pos.iloc[index] = sentimentResults['pos']
+    df.compound.iloc[index] = sentimentResults['compound']
+
 print(df)
