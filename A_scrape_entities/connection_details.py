d34c4228df7f6d33d3b10add815e6c9f621a6855
nick11337 <muellernick1994@gmail.com>
2021-11-17 10:25:03 +0100

    deleted conn details

--- A_scrape_entities/connection_details.py
+++
@@ -1,7 +0,0 @@
-CONNECTION = {
-"client_id": "u6wgxN-F0fh4aJyHd7Jm3w",
-"client_secret": "A8FQCMsESq3wEU8HYULXETpyajkkhg",
-"username": "Rita0ral",
-"password": "heppie94",
-"user_agent": "WSBScrapingV1",
-}
